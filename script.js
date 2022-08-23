abce377c2b39544423efdb6d94b8486ce07a921d
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-08-23 11:28:57 +0600

    validation added

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
 
+function display(getAllPlayer) {
+    const playerLoaction = document.getElementById('player-add');
+    playerLoaction.innerHTML = '';
+    for (let i = 0; i < getAllPlayer.length; i++) {
+        const singlePlayer = getAllPlayer[i].playername;
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${singlePlayer}</td>
+        `
+        if (i >= 5) {
+            alert('this');
+            break;
 
+        }
+        else {
+            playerLoaction.appendChild(tr);
+        }
 
+    }
 
+}
 
+let array = [];
 
+function getPlayerName(element) {
+    const parent = element.parentNode.parentNode.children[0].innerText;
+    const playerObject = {
+        playername: parent,
+    }
+    array.push(playerObject)
+    if (array.length >= 6) {
+    }
+    else {
+        const totalPlayer = document.getElementById('player-length').innerText = array.length;
 
+    }
+    display(array);
+}
 document.getElementById('per-player').addEventListener('click', function () {
     const budgetMoneyValueToNum = getInputID('budget-money')
-    const budgetTotal = budgetMoneyValueToNum * 5;
+    const playerLength = parseInt(getInner('player-length'))
+    const budgetTotal = budgetMoneyValueToNum * playerLength;
     if (isNaN(budgetMoneyValueToNum)) {
         alert('please enter an valid input')
-
     }
     else {
         getSet('player-total', budgetTotal);
-
     }
 
-
 })
+
+
+
 document.getElementById('calculate-total').addEventListener('click', function () {
     const getMangerMoney = getInputID('manager-money');
     const getCoachmMoney = getInputID('coach-money');
-    const getExpensesone = document.getElementById('player-total');
-    const getExpensesInner = getExpensesone.innerText;
-    const getExpensesInnerToNum = parseInt(getExpensesInner);
-    const finalTotal = getMangerMoney + getCoachmMoney + getExpensesInnerToNum;
+    const getExpensesone = parseInt(getInner('player-total'));
+    const finalTotal = getMangerMoney + getCoachmMoney + getExpensesone;
     if (isNaN(finalTotal)) {
         alert('please enter an valid input')
     }
@@ -34,3 +66,4 @@ document.getElementById('calculate-total').addEventListener('click', function ()
 
     }
 })
+'player-length'
--- utilities.js
+++ utilities.js
@@ -1,7 +1,10 @@
+// sortcut utilities
+
 function getInputID(elementId) {
     const budgetMoney = document.getElementById(elementId);
     const budgetMoneyValue = budgetMoney.value;
     const budgetMoneyValueToNum = parseInt(budgetMoneyValue);
+    budgetMoney.value = '';
     return budgetMoneyValueToNum;
 }
 function getSet(getId, setId) {
@@ -9,35 +12,10 @@ function getSet(getId, setId) {
     getExpenses.innerText = setId;
 }
 
-
-
-
-
-function display(getAllPlayer) {
-    const playerLoaction = document.getElementById('player-add');
-    playerLoaction.innerHTML = '';
-    for (let i = 0; i < getAllPlayer.length; i++) {
-        const singlePlayer = getAllPlayer[i].playername;
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
-        <th>${i + 1}</th>
-        <td>${singlePlayer}</td>
-        `
-        playerLoaction.appendChild(tr);
-    }
+function getInner(innerId) {
+    const playerLength = document.getElementById(innerId);
+    const playerLengthToNum = playerLength.innerText;
+    return playerLengthToNum;
 }
 
 
-
-
-let array = [];
-
-function getPlayerName(element) {
-    const parent = element.parentNode.parentNode.children[0].innerText;
-    const playerObject = {
-        playername: parent,
-    }
-    array.push(playerObject)
-    document.getElementById('player-length').innerText = array.length;
-    display(array);
-}   
